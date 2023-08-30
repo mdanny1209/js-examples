c5ddc8ae87518d8adc7f03da1328cd56eb2c2061
dizz02 <sergioargondizzo@gmail.com>
2023-08-30 09:24:33 +0200

    333

--- src/components/AccountInfo/AccountInfo.tsx
+++ src/components/AccountInfo/AccountInfo.tsx
@@ -1,73 +1,59 @@
-import { useEffect, useState } from 'react';
-import { connect, getSigningCosmWasmClient, WalletConnect } from '@sei-js/core';
+import {useEffect, useState} from 'react'
+import {connect, getSigningCosmWasmClient, WalletConnect} from '@sei-js/core'
 import { coins } from '@cosmjs/amino';
-import './styles.css';
+import './styles.css'
 
 function App() {
-  const [count, setCount] = useState(0);
-  const [wallet, setWallet] = useState<WalletConnect>();
+    const [count, setCount] = useState(0)
+    const [wallet, setWallet] = useState<WalletConnect>()
 
-  useEffect(() => {
-    connect('keplr', 'pacific-1').then(setWallet);
-  }, []);
+    useEffect(() => {
+        connect('keplr', 'pacific-1').then(setWallet)
+    }, [])
 
-  useEffect(() => {
-    fetchCount().then(setCount);
-  }, [wallet]);
+    useEffect(() => {
+        fetchCount().then(setCount)
+    }, [wallet])
 
-  const fetchCount = async () => {
-    if (!wallet) return;
-
-    const client = await getSigningCosmWasmClient(
-      'https://sei-rpc.polkachu.com/',
-      wallet.offlineSigner
-    );
-    const response = await client.queryContractSmart(
-      'sei12uzyf3gkeehdgzuqzhy6nk2039s7l2kre0sknj73c4ngy53klq4qpgpvz6',
-      { get_count: {} }
-    );
-    return response.count;
+    const fetchCount = async () => {
+      if(!wallet) return;
+  
+      const client = await getSigningCosmWasmClient("https://sei-rpc.polkachu.com/", wallet.offlineSigner);
+      const response = await client.queryContractSmart("sei12uzyf3gkeehdgzuqzhy6nk2039s7l2kre0sknj73c4ngy53klq4qpgpvz6", {get_count: {}})
+      return response.count;
   };
 
-  const claimTokens = async () => {
-    if (!wallet) return;
-
-    const client = await getSigningCosmWasmClient(
-      'https://sei-rpc.polkachu.com/',
-      wallet.offlineSigner
-    );
+ const incrementCounter = async () => {
+    if(!wallet) return;
+        
+    const client = await getSigningCosmWasmClient("https://sei-rpc.polkachu.com/", wallet.offlineSigner);
 
     const senderAddress = wallet.accounts[0].address;
+    const contractAddress = "sei12uzyf3gkeehdgzuqzhy6nk2039s7l2kre0sknj73c4ngy53klq4qpgpvz6";
+
 
     const msg = {
-      claim: {},
+        "claim": {},
+        
     };
 
-    const fee = {
-      amount: coins(24000, 'usei'),
-      gas: '200000',
+    const fee = { 
+       amount: coins(24000, 'usei'),
+       gas: "200000"
     };
 
-    const sendAmount = coins(1, 'usei');
-
-    const response = await client.execute(
-      senderAddress,
-      'sei12uzyf3gkeehdgzuqzhy6nk2039s7l2kre0sknj73c4ngy53klq4qpgpvz6',
-      msg,
-      fee,
-      sendAmount.toString() // Include the sendAmount here
-    );
-
+    const response = await client.execute(senderAddress, "sei12uzyf3gkeehdgzuqzhy6nk2039s7l2kre0sknj73c4ngy53klq4qpgpvz6", msg, fee)
+    
     // Updates the counter state again
     await fetchCount();
-  };
-
-  return (
-    <div>
-      <h1>Count is: {count}</h1>
-      <button onClick={claimTokens}>Claim</button>
-    </div>
-  );
 }
 
-export default App;
+    return (
+        <div>
+            <h1>Count is: {count}</h1>
+            <button onClick={incrementCounter}>claim</button>
+        </div>
+    )
+}
+
+export default App
